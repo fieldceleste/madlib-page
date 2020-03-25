e4b27d7e66030d57e52b0259962649597abdb469
apple <apple@Celestes-IMac.local>
2020-03-25 11:24:13 -0700

    finish styling page and fixed grammatical errors

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
     var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var action1Input = $("input#action1").val();
+    var action2Input = $("input#action2").val();
+    var action3Input = $("input#action3").val();
+    var action4Input = $("input#action4").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    $(".action1").text(action1Input);
+    $(".action2").text(action2Input);
+    $(".action3").text(action3Input);
+    $(".action4").text(action4Input);
+    
     $("#story").show();
 
     event.preventDefault();
